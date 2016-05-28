7327b67012bdeaea8c64bdcb108bf11b921c49f2
toklok <joecurt85@gmail.com>
2016-05-28 16:00:11 -0400

    first commit for using clojure as preprocessor

---
+++ README.md
@@ -0,0 +1,22 @@
+# Using Garden CSS to build CSS games
+
+This was inspired because I am trying to learn Clojure and because of this (article)[https://una.im/css-games/#%F0%9F%92%81] on making
+a CSS only game using sass.
+
+## Installation
+
+Clone repo.
+
+Install (http://leiningen.org/)[leiningen]
+
+## Usage
+
+## Options
+
+`lein garden once`
+or
+`lein garden auto` to update changes
+
+## Examples
+
+...
---
+++ project.clj
@@ -0,0 +1,24 @@
+(defproject garden-css "0.1.0-SNAPSHOT"
+  :plugins [[lein-garden "0.2.8"]]
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                          [garden "1.3.2"]
+                          [me.raynes/fs "1.4.6"]
+                          [ns-tracker "0.3.0"]]
+  :main ^:skip-aot garden-css.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}}
+  :garden {:builds [{;; Optional name of the build:
+                     :id "screen"
+                     ;; Source paths where the stylesheet source code is
+                     :source-paths ["src"]
+                     ;; The var containing your stylesheet:
+                     :stylesheet garden-css.core/screen
+                     ;; Compiler flags passed to `garden.core/css`:
+                     :compiler {;; Where to save the file:
+                                :output-to "resources/screen.css"
+                                ;; Compress the output?
+                                :pretty-print? false}}]})
